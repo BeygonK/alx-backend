3b71014603181136a68f7fb9cfaa022fa334422f
BeygonK <kipkoechbeygon@gmail.com>
2024-08-28 11:04:04 +0300

    pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -34,10 +34,8 @@ class Server:
                  page_size: int = 10) -> List[List]:
         """Return the appropriate page of the dataset.
         """
-        assert isinstance(page, int)
-        and page > 0, "Page must be a positive integer."
-        assert isinstance(page_size, int)
-        and page_size > 0, "Page size must be a positive integer."
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
