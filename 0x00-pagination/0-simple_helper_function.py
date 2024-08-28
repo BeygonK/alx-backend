9d36ddde469f4139a31d821d6f425143d5d307df
BeygonK <kipkoechbeygon@gmail.com>
2024-08-28 10:48:50 +0300

    helper

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """This module has a helper function"""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """This function returns a tuple"""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
